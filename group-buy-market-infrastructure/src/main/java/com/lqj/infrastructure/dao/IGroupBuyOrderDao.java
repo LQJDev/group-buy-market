b08f76c9175d13b4426a995849619aec9fbc1038
wssblqj <1340935597@qq.com>
2026-01-31 10:00:46 +0800

    feat: 2-19: ELK + AI MCP 检索

--- group-buy-market-infrastructure/src/main/java/com/lqj/infrastructure/dao/IGroupBuyOrderDao.java
+++ group-buy-market-infrastructure/src/main/java/com/lqj/infrastructure/dao/IGroupBuyOrderDao.java
@@ -3,6 +3,7 @@ package com.lqj.infrastructure.dao;
 
 import com.lqj.infrastructure.dao.po.GroupBuyOrder;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 import java.util.Set;
@@ -29,11 +30,11 @@ public interface IGroupBuyOrderDao {
 
     int updateOrderStatus2COMPLETE(String teamId);
 
-    List<GroupBuyOrder> queryGroupBuyProgressByTeamIds(Set<String> teamIds);
+    List<GroupBuyOrder> queryGroupBuyProgressByTeamIds(@Param("teamIds") Set<String> teamIds);
 
-    Integer queryAllTeamCount(Set<String> teamIds);
+    Integer queryAllTeamCount(@Param("teamIds")Set<String> teamIds);
 
-    Integer queryAllTeamCompleteCount(Set<String> teamIds);
+    Integer queryAllTeamCompleteCount(@Param("teamIds") Set<String> teamIds);
 
-    Integer queryAllUserCount(Set<String> teamIds);
+    Integer queryAllUserCount(@Param("teamIds") Set<String> teamIds);
 }
